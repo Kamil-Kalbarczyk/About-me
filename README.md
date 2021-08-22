c01c73f580ad5f4b2e2302526f070d398e905d0a
Kamil Kalbarczyk <k.kalbarczyk1@gmail.com>
2021-08-22 19:49:45 +0200

    JS code refactor

--- README.md
+++ README.md
@@ -1,15 +1,9 @@
 # This is my first project
 
-# Frontend Mentor - FAQ accordion card solution
-
-This is a solution to the [FAQ accordion card challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/faq-accordion-card-XlyjD0Oam). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
-
 ### Built with
 
-- Semantic HTML5 markup
-- CSS custom properties
-- Flexbox
-- CSS Grid
-- JavaScript (basics)
+- HTML5
+- SCSS: position absolute, flexbox, keyframes, media queries
+- JavaScript: if, for, anddEventListener, change classes, change textContent
 
-#### Source: https://www.frontendmentor.io/solutions/faq-accordion-card-GyNuYUMvb
+#### Source of project (only idea of layout and graphics): https://www.frontendmentor.io/solutions/faq-accordion-card-GyNuYUMvb
--- js/code.js
+++ js/code.js
@@ -22,157 +22,104 @@ const answerFifth = document.querySelector('p.answer-fifth');
 
 const answers = [answerFirst, answerSecond, answerThird, answerFourth, answerFifth];
 
+
+// Questions work
+
 caseFirst.addEventListener('click', () => {
-    for (i = 0; i < cases.length; i++) {
-        cases[i].classList.remove('active');
-    };
-
-    for (i = 0; i < questions.length; i++) {
-        questions[i].classList.remove('active');
-    };
-
-    for (i = 0; i < answers.length; i++) {
-        answers[i].classList.remove('active');
-    }
-    // console.log("działa");
     if (caseFirst.classList.contains('active')) {
         caseFirst.classList.remove('active');
         questionFirst.classList.remove('active');
         answerFirst.classList.remove('active');
     } else {
+
+        for (i = 0; i < cases.length; i++) {
+            cases[i].classList.remove('active');
+        };
+
+        for (i = 0; i < questions.length; i++) {
+            questions[i].classList.remove('active');
+        };
+
+        for (i = 0; i < answers.length; i++) {
+            answers[i].classList.remove('active');
+        };
         caseFirst.classList.add('active');
         questionFirst.classList.add('active');
         answerFirst.classList.add('active');
     }
-
-    // caseSecond.classList.remove('active');
-    // caseThird.classList.remove('active');
-    // caseFourth.classList.remove('active');
-    // caseFifth.classList.remove('active');
-
-    // questionSecond.classList.remove('active');
-    // questionThird.classList.remove('active');
-    // questionFourth.classList.remove('active');
-    // questionFifth.classList.remove('active');
-
-    // answerSecond.classList.remove('active');
-    // answerThird.classList.remove('active');
-    // answerFourth.classList.remove('active');
-    // answerFifth.classList.remove('active');
 });
 
 caseSecond.addEventListener('click', () => {
-    for (i = 0; i < cases.length; i++) {
-        cases[i].classList.remove('active');
-    };
-
-    for (i = 0; i < questions.length; i++) {
-        questions[i].classList.remove('active');
-    };
-
-    for (i = 0; i < answers.length; i++) {
-        answers[i].classList.remove('active');
-    }
-
     if (caseSecond.classList.contains('active')) {
         caseSecond.classList.remove('active');
         questionSecond.classList.remove('active');
         answerSecond.classList.remove('active');
     } else {
+
+        for (i = 0; i < cases.length; i++) {
+            cases[i].classList.remove('active');
+        };
+
+        for (i = 0; i < questions.length; i++) {
+            questions[i].classList.remove('active');
+        };
+
+        for (i = 0; i < answers.length; i++) {
+            answers[i].classList.remove('active');
+        };
         caseSecond.classList.add('active');
         questionSecond.classList.add('active');
         answerSecond.classList.add('active');
     }
-    // caseFirst.classList.remove('active');
-    // caseThird.classList.remove('active');
-    // caseFourth.classList.remove('active');
-    // caseFifth.classList.remove('active');
-
-    // questionFirst.classList.remove('active');
-    // questionThird.classList.remove('active');
-    // questionFourth.classList.remove('active');
-    // questionFifth.classList.remove('active');
-
-    // answerFirst.classList.remove('active');
-    // answerThird.classList.remove('active');
-    // answerFourth.classList.remove('active');
-    // answerFifth.classList.remove('active');
 });
 
 caseThird.addEventListener('click', () => {
-    for (i = 0; i < cases.length; i++) {
-        cases[i].classList.remove('active');
-    };
-
-    for (i = 0; i < questions.length; i++) {
-        questions[i].classList.remove('active');
-    };
-
-    for (i = 0; i < answers.length; i++) {
-        answers[i].classList.remove('active');
-    }
-
     if (caseThird.classList.contains('active')) {
         caseThird.classList.remove('active');
         questionThird.classList.remove('active');
         answerThird.classList.remove('active');
     } else {
+        for (i = 0; i < cases.length; i++) {
+            cases[i].classList.remove('active');
+        };
+
+        for (i = 0; i < questions.length; i++) {
+            questions[i].classList.remove('active');
+        };
+
+        for (i = 0; i < answers.length; i++) {
+            answers[i].classList.remove('active');
+        };
         caseThird.classList.add('active');
         questionThird.classList.add('active');
         answerThird.classList.add('active');
     }
-    // caseFirst.classList.remove('active');
-    // caseSecond.classList.remove('active');
-    // caseFourth.classList.remove('active');
-    // caseFifth.classList.remove('active');
-
-    // questionFirst.classList.remove('active');
-    // questionSecond.classList.remove('active');
-    // questionFourth.classList.remove('active');
-    // questionFifth.classList.remove('active');
-
-    // answerFirst.classList.remove('active');
-    // answerSecond.classList.remove('active');
-    // answerFourth.classList.remove('active');
-    // answerFifth.classList.remove('active');
 });
 
 caseFourth.addEventListener('click', () => {
-    for (i = 0; i < cases.length; i++) {
-        cases[i].classList.remove('active');
-    };
-
-    for (i = 0; i < questions.length; i++) {
-        questions[i].classList.remove('active');
-    };
-
-    for (i = 0; i < answers.length; i++) {
-        answers[i].classList.remove('active');
-    };
-
     if (caseFourth.classList.contains('active')) {
         caseFourth.classList.remove('active');
         questionFourth.classList.remove('active');
         answerFourth.classList.remove('active');
     } else {
+
+        for (i = 0; i < cases.length; i++) {
+            cases[i].classList.remove('active');
+        };
+
+        for (i = 0; i < questions.length; i++) {
+            questions[i].classList.remove('active');
+        };
+
+        for (i = 0; i < answers.length; i++) {
+            answers[i].classList.remove('active');
+        };
+
         caseFourth.classList.add('active');
         questionFourth.classList.add('active');
         answerFourth.classList.add('active');
-    };
-    // caseFirst.classList.remove('active');
-    // caseSecond.classList.remove('active');
-    // caseThird.classList.remove('active');
-    // caseFifth.classList.remove('active');
 
-    // questionFirst.classList.remove('active');
-    // questionSecond.classList.remove('active');
-    // questionThird.classList.remove('active');
-    // questionFifth.classList.remove('active');
-
-    // answerFirst.classList.remove('active');
-    // answerSecond.classList.remove('active');
-    // answerThird.classList.remove('active');
-    // answerFifth.classList.remove('active');
+    }
 });
 
 caseFifth.addEventListener('click', () => {
@@ -197,29 +144,6 @@ caseFifth.addEventListener('click', () => {
         questionFifth.classList.add('active');
         answerFifth.classList.add('active');
     };
-    // if (caseFifth.classList.contains('active')) {
-    //     caseFifth.classList.remove('active');
-    //     questionFifth.classList.remove('active');
-    //     answerFifth.classList.remove('active');
-    // } else {
-    //     caseFifth.classList.add('active');
-    //     questionFifth.classList.add('active');
-    //     answerFifth.classList.add('active');
-    // }
-    // caseFirst.classList.remove('active');
-    // caseThird.classList.remove('active');
-    // caseFourth.classList.remove('active');
-    // caseSecond.classList.remove('active');
-
-    // questionFirst.classList.remove('active');
-    // questionThird.classList.remove('active');
-    // questionFourth.classList.remove('active');
-    // questionSecond.classList.remove('active');
-
-    // answerFirst.classList.remove('active');
-    // answerSecond.classList.remove('active');
-    // answerThird.classList.remove('active');
-    // answerFourth.classList.remove('active');
 });
 
 
